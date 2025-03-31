70b7aa930d3218446e5079bf411fbd1f2c670041
Gayanthika Shankar <gayanthika23shankar@gmail.com>
2025-03-31 19:00:25 +0530

    update

---
+++ python/practice/scope.py
@@ -0,0 +1,24 @@
+#var scope=where a var is visible and accesible
+#cope resol: LEGB -> local-enclosed-global-builtin
+
+#INCOMPLETE
+
+'''
+#1
+def f1():
+    a=1 #variables declared inside a fn have a local scope
+    print(a)
+
+def f2():
+    b=2
+    print(b) 
+    ''''
+
+#2
+def f1():
+    x=1  
+    print(x)
+
+def f2():
+    x=2
+    print(x)
