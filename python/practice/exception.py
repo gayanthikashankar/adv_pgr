429991d92c9eec62778db3b8034abe433c7c1113
Gayanthika Shankar <gayanthika23shankar@gmail.com>
2025-04-29 00:36:54 +0530

    update readme

---
+++ python/practice/exception.py
@@ -0,0 +1,34 @@
+#exception = event detet during during pgr exec that interrupts flow of pgr
+'''
+numerator = int(input("eneter a number as numerator: "))
+denominator = int(input("eneter a number as denominator: "))
+res = numerator/denominator
+print(res)
+'''
+
+'''
+eneter a number as numerator: 5
+eneter a number as denominator: 0
+Traceback (most recent call last):
+    res = numerator/denominator
+          ~~~~~~~~~^~~~~~~~~~~~
+ZeroDivisionError: division by zero
+
+'''
+try:
+    numerator = int(input("eneter a number as numerator: "))
+    denominator = int(input("eneter a number as denominator: "))
+    res = numerator/denominator
+except ZeroDivisionError as e:
+    print("cannot divide by 0")
+    print(e)
+except ValueError as e:
+    print("enter only numbers")
+    print(e)
+except Exception as e:
+    print("there was an issue")
+    print(e) #optional
+else:
+    print(res)
+finally:
+    print("this will always execute")
